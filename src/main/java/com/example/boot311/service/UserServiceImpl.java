e513cb8220fa02015d0a0e608ef935111de92772
aurum447 <aurum447realme2020@gmail.com>
2022-10-24 17:03:09 +0300

    ,

--- src/main/java/com/example/boot311/service/UserServiceImpl.java
+++ src/main/java/com/example/boot311/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.boot311.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -18,21 +19,25 @@ public class UserServiceImpl implements UserService{
         this.userRepository = userRepository;
     }
 
+    @Transactional
     @Override
     public User findById(int id) {
         return userRepository.getOne(id);
     }
 
+    @Transactional
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    @Transactional
     @Override
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
+    @Transactional
     @Override
     public void deleteById(int id) {
         userRepository.deleteById(id);
